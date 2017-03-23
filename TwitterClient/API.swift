2ed7fc83a3076c3e71d65dcb1623188b4f497267
Kyle Hillman <khillman84@gmail.com>
2017-03-23 15:10:48 -0700

    worked through lab in class

--- TwitterClient/API.swift
+++ TwitterClient/API.swift
@@ -73,10 +73,10 @@ class API {
         
     }
     
-    private func updateTimeLine(callback: @escaping TweetsCallback){
-        let url = URL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")
+    private func updateTimeLine(url: String, callback: @escaping TweetsCallback){
+//        let url = URL(string: "")
         
-        if let request = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: .GET, url: url, parameters: nil){
+        if let request = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: .GET, url: URL(string: url), parameters: nil){
             request.account = self.account
             request.perform(handler: { (data, response, error) in
                 if let error = error {
@@ -112,13 +112,24 @@ class API {
             login(callback: { (account) in
                 if let account = account {
                     self.account = account
-                    self.updateTimeLine(callback: { (tweets) in
+                    self.updateTimeLine(url: "https://api.twitter.com/1.1/statuses/home_timeline.json", callback: { (tweets) in
                         callback(tweets)
                     })
                 }
             })
         } else {
-            self.updateTimeLine(callback: callback)
+            self.updateTimeLine(url: "https://api.twitter.com/1.1/statuses/home_timeline.json", callback: { (tweets) in
+                callback(tweets)
+            })
+        }
+    }
+    
+    func getTweetsFor(_ user: String, callback: @escaping TweetsCallback){
+        
+        let urlString = "https: //api.twitter.com/1.1/statuses/user_timeline.json?screen_name=\(user)"
+        
+        self.updateTimeLine(url: urlString) { (tweets) in
+            callback(tweets)
         }
     }
     
