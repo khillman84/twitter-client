f572b8e2e19449d8bd3a06259b08f9ba853eea11
Kyle Hillman <khillman84@gmail.com>
2017-03-22 17:01:22 -0700

    text displaying on segue view

--- TwitterClient/TweetDetailViewController.swift
+++ TwitterClient/TweetDetailViewController.swift
@@ -11,13 +11,19 @@ import UIKit
 class TweetDetailViewController: UIViewController {
     
     var tweet : Tweet!
-
+    
+    @IBOutlet weak var singleTweet: UILabel!
+    @IBOutlet weak var userName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(self.tweet.user?.name ?? "Unkown")
         print(self.tweet.text)
+        
+        singleTweet.text = self.tweet.text
+        userName.text = self.tweet.user?.name
     }
 
-
+    
 }
